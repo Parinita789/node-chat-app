05f26b726adda747923f3b732a2b5ec75d08d9b7
Parinita <parinitamehta123@gmail.com>
2018-04-22 23:26:07 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var publicPath = path.join(__dirname, '../public');
+var port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+app.listen(port, () => {
+	console.log(`app listening on ${ port }`);
+})
